419d2508fe9f454b55a6f85a7a145e04c26d8f36
NizarZouabi <136129653+NizarZouabi@users.noreply.github.com>
2023-11-11 13:56:38 +0100

    #

---
+++ React/Practice/first_JSX/first_jsx/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <h1>Hello Dojo!</h1>
+          <h2>Things i need to do:</h2>
+          <ul className="list">
+            <li>Learn React</li>
+            <li>Climb Mt. Everest</li>
+            <li>Run a marathon</li>
+            <li>Feed the cats</li>
+        </ul>
+      </header>
+      </div>
+  );
+}
+
+export default App;
